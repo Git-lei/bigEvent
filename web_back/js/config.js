77303d8b575fd7e061f36411c4916ec9447cf2f0
Git-lei <Fluoxetine_Si@163.com>
2019-12-17 21:54:48 +0800

    继续完善

--- web_back/js/config.js
+++ web_back/js/config.js
@@ -7,3 +7,6 @@ var LOGOUT = BASIC_URL + '/admin/logout';
 var GETUSER = BASIC_URL + '/admin/getuser';
 var GETTOTAL = BASIC_URL + '/admin/article_count';
 var GETCOMMENT = BASIC_URL + '/admin/comment_count';
+var GET_USERINFO = BASIC_URL + '/admin/userinfo_get';
+var USERINFO_EDIT = BASIC_URL + '/admin/userinfo_edit';
+var CATEGORY_SEARCH = BASIC_URL + '/admin/category_search';
--- web_back/js/user.js
+++ web_back/js/user.js
@@ -1,4 +1,6 @@
 var user = {
+
+    //登录
     login: function (options) {
         $.ajax({
             type: 'post',
@@ -20,7 +22,7 @@ var user = {
         });
     },
 
-
+    //退出
     logout: function (options) {
         $.ajax({
             type: 'post',
@@ -36,6 +38,8 @@ var user = {
         })
     },
 
+
+    //获取用户信息
     getUser: function (options) {
         $.ajax({
             type: 'get',
@@ -53,6 +57,48 @@ var user = {
         })
     },
 
+    //iframe 标签实现 页面嵌套
+
+
+    //用户详细信息获取展示
+    getUserinfo: function (options) {
+        $.ajax({
+            url: GET_USERINFO,
+            success: function (res) {
+                if (res.code === 200) {
+                    //将获取到的数据展示到 页面里
+                    //  遍历
+                    options.success(res);
+                }
+            }
+        })
+
+    },
+
+
+    //用户详细信息 修改
+    userInfoEdit: function (options) {
+        $.ajax({
+            type: 'post',
+            url: USERINFO_EDIT,
+            data: options.data,
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                }
+
+            }
+        })
+
+    },
+
+
+
+
+
+    //获取文章数
     gettotal: function (options) {
         $.ajax({
             type: 'get',
@@ -68,6 +114,9 @@ var user = {
             }
         })
     },
+
+
+    //获取评论数
     getcomment: function (options) {
         $.ajax({
             type: 'get',
@@ -81,9 +130,77 @@ var user = {
                 }
             }
         })
+    },
+
+
+    //获取月增文章数
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //文章类别
+    category: function (options) {
+        $.ajax({
+            type: 'get',
+            url: CATEGORY_SEARCH,
+            success: function (res) {
+                console.log(res.data);
+
+                if (res.code === 200) {
+                    options.success(res);
+                }
+            }
+        })
+    },
+
+
+    //新增类别
+    addCategory: function (options) {
+        $.ajax({
+            type: 'post',
+            url: 'http://localhost:8000/admin/category_add',
+            data: options.fd,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+
+            }
+        })
     }
-
-
 
 
 
